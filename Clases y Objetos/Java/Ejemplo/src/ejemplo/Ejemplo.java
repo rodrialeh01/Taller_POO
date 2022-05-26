45e0c35dd68c527d4ec7b1681b2f695874aee6c0
rodrialeh01 <2823897250101@ingenieria.usac.edu.gt>
2022-05-26 17:25:10 -0600

    Reordenando carpetas

--- Clases y Objetos/Java/Ejemplo/src/ejemplo/Ejemplo.java
+++ Clases y Objetos/Java/Ejemplo/src/ejemplo/Ejemplo.java
@@ -3,8 +3,10 @@ package ejemplo;
 public class Ejemplo {
 
     public static void main(String[] args) {
+        //CREANDO OBJETOS
         Pelicula peli1 = new Pelicula("Venom","Adolescentes", 5.00, 20,1);
         Pelicula peli2 = new Pelicula("Madagascar", "Todos",4.50, 10,2);
+        //MOSTRANDO OBJETOS
         System.out.println("NOMBRE: " + peli1.getNombre());
         System.out.println("CANTIDAD: " + peli1.getCantidad());
         System.out.println("CODIGO: " + peli1.getCodigo());
@@ -12,6 +14,7 @@ public class Ejemplo {
         System.out.println("NOMBRE: " + peli2.getNombre());
         System.out.println("PRECIO: " + peli2.getPrecio());
         System.out.println("================================");
+        //ACTUALIZANDO OBJETOS
         peli1.setNombre("Venom 2");
         peli1.setPrecio(10.0);
         System.out.println("================================");
